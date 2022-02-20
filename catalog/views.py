e4b75414ded0baba53ac19997f9d07f92e76f761
jensh <jens@petja.de>
2022-02-20 18:56:16 +0100

    1.Version

---
+++ catalog/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from django.views import generic
+
+from catalog.models import Book, BookInstance, Author, Genre
+
+FILTER_BOOKS: str = 'die'
+FILTER_GENRES: str = 'poetry'
+
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    num_books_contain = Book.objects.filter(title__icontains=FILTER_BOOKS).count()
+    num_genres_contain = Genre.objects.filter(name__icontains=FILTER_GENRES).count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_books_contain': num_books_contain,
+        'num_genres_contain': num_genres_contain,
+        'FILTER_BOOKS': FILTER_BOOKS,
+        'FILTER_GENRES': FILTER_GENRES,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+
+    # context_object_name = 'my_book_list'   # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains=FILTER_BOOKS)[:5]  # Get 5 books containing the title war
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains=FILTER_BOOKS)[:5]  # Get 5 books containing the title war
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+class AuthorListView(generic.ListView):
+    model = Author
---
+++ primer/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from primer.models import MyModelName
+
+admin.site.register(MyModelName)
