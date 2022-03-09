69902c065e4a3a0a8daa2677d38948a8b34e1bac
jensh <jens@petja.de>
2022-03-09 20:05:46 +0100

    6.7 .env

--- w_library/settings.py
+++ w_library/settings.py
@@ -138,6 +138,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'jhsoft1@gmail.com'
-EMAIL_HOST_PASSWORD = int(os.environ.get('TIMES', '3'))
+EMAIL_HOST_PASSWORD = os.environ.get('TIMES')
 # USE_L10N = True
 django_heroku.settings(locals())
